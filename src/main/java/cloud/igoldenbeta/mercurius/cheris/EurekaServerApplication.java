598b9624d752e535cc9b90d299955f9356ee522d
ruofei <hanrf@igoldenbeta.com>
2016-09-02 14:36:17 +0800

    first blood

---
+++ src/main/java/cloud/igoldenbeta/mercurius/cheris/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package cloud.igoldenbeta.mercurius.cheris;
+
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+  public static void main(String[] args) throws IOException {
+    SpringApplication.run(EurekaServerApplication.class, args);
+  }
+}
